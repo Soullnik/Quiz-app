8d71ecd24995a2e0efd834c45a668e1c12a2bb2e
soullnik <soullnik@gmail.com>
2019-10-07 16:39:54 +0300

    dfgdfgdfg
    dfg

---
+++ src/containers/TopAnswerPeople/TopAnswerPeople.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class TopAnswerPeople extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Рейтинг</h1>
+            </div>
+        )
+    }
+}
